6b1e6c496c026d964cb5512a19fc469205270da2
Michael DeFreitas <mikeylove@gmail.com>
2024-06-01 11:07:53 -0700

    new utility `Polo` to declare Plain Old Lua Objects
    ...in the style of our time

---
+++ reascripts/common/libs/Polo.lua
@@ -0,0 +1,29 @@
+--[[
+
+  Polo.lua - Plain Old Lua Object (POLO) class generator
+
+]]--
+
+function Polo(definition)
+  definition.__index = definition
+
+  local new_override = definition.new
+
+  definition.new = function(...)
+    local o
+
+    if new_override then
+      local args = table.pack(...)
+      o = new_override(table.unpack(args))
+    else
+      o = ... or {}
+    end
+
+    setmetatable(o, definition)
+    if o.init then o:init() end
+
+    return o
+  end
+
+  return definition
+end
