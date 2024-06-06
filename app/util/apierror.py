1c10f28c130658f9b2c832857b19d2ac30f1d97d
Dave Benjamin <dave@teamaud.io>
2024-06-06 13:25:35 -0700

    Standardize API error structure

---
+++ app/util/apierror.py
@@ -0,0 +1,14 @@
+from fastapi.responses import JSONResponse
+
+class APIError(Exception):
+    def __init__(self, error):
+        self.error = error
+
+    def to_response(self):
+        return error_response(self.error)
+
+def error_dict(error):
+    return {"error": repr(error)}
+
+def error_response(error):
+    return JSONResponse(status_code=500, content=error_dict(error))
