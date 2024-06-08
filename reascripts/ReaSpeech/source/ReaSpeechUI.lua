8b77e62fe0b3b0cf6286480cd2b168da2a7f0396
Michael DeFreitas <mikeylove@gmail.com>
2024-06-07 19:38:35 -0700

    extract IntervalFunction class

--- reascripts/ReaSpeech/source/ReaSpeechUI.lua
+++ reascripts/ReaSpeech/source/ReaSpeechUI.lua
@@ -178,17 +178,6 @@ function ReaSpeechUI:interval_functions()
   end
 
   self._interval_functions = {
-    -- IntervalFunction.new(5, function()
-    --   -- run no more often than once every 5 seconds
-    --   -- chill interval to check on states that don't
-    --   -- need to feel so snappy
-    -- end),
-
-    -- IntervalFunction.new(-15, function ()
-    --   -- run every 15 ticks, ~0.5 seconds
-    --   -- maybe a good interval for updating some states
-    --   -- in a way that feels responsive, like selections
-    -- end)
   }
 
   return self._interval_functions
@@ -198,32 +187,6 @@ function ReaSpeechUI:handle_interval_functions(time)
   local fs = self:interval_functions()
   for i = 1, #fs do
     fs[i]:react(time)
-  end
-end
-
-IntervalFunction = Polo {
-  new = function(interval, f)
-    return {
-      interval = interval,
-      f = f,
-      last = 0
-    }
-  end
-}
-
-function IntervalFunction:react(time)
-  if self.interval >= 0 then
-    if time - self.last >= self.interval then
-      self.f()
-      self.last = time
-    end
-  else
-    self.last = self.last - 1
-
-    if self.last < self.interval then
-      self.f()
-      self.last = 0
-    end
   end
 end
 
