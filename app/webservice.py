24cd414813afedff79a00a4a5f2d56be41f2d7e7
Dave Benjamin <dave@teamaud.io>
2024-08-10 08:33:38 -0700

    Display correct filename in web content

--- app/webservice.py
+++ app/webservice.py
@@ -110,6 +110,12 @@ output_directory = os.environ.get("OUTPUT_DIRECTORY", os.getcwd() + "/app/output
 output_url_prefix = os.environ.get("OUTPUT_URL_PREFIX", "/output")
 app.mount(output_url_prefix, StaticFiles(directory=output_directory), name="output")
 
+def reascript_filename(name):
+    if APP_ENV == 'production':
+        return f'{name}.lua'
+    else:
+        return f'{name}-{APP_ENV}.lua'
+
 @app.exception_handler(apierror.APIError)
 async def api_exception_handler(request: Request, exc: apierror.APIError):
     return exc.to_response()
@@ -124,14 +130,15 @@ async def index():
 
 @app.get("/reaspeech", response_class=HTMLResponse, include_in_schema=False)
 async def reaspeech(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "docs_url": docs_url})
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "reascript_filename": reascript_filename("ReaSpeech"),
+        "docs_url": docs_url,
+    })
 
 @app.get("/reascript", response_class=PlainTextResponse, include_in_schema=False)
 async def reascript(request: Request, name: str, host: str, protocol: str):
-    if APP_ENV == 'production':
-        filename = f'{name}.lua'
-    else:
-        filename = f'{name}-{APP_ENV}.lua'
+    filename = reascript_filename(name)
     return templates.TemplateResponse("reascript.lua", {
             "request": request,
             "name": name,
