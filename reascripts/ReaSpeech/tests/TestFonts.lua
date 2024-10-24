ebc4e1c5e1863c17cf5f1a5d305f18f8ffb0b8a9
Michael DeFreitas <mikeylove@gmail.com>
2024-10-24 07:54:03 -0700

    forgot to git add TestFonts.lua

---
+++ reascripts/ReaSpeech/tests/TestFonts.lua
@@ -0,0 +1,113 @@
+package.path = '../common/libs/?.lua;../common/vendor/?.lua;' .. package.path
+
+local lu = require('luaunit')
+
+require('mock_reaper')
+require('source/Fonts')
+
+--
+
+TestFonts = {}
+
+function TestFonts:testWrap()
+  local old_imgui = ImGui or {}
+
+  local push_called = false
+  local pop_called = false
+  ImGui.PushFont = function(_, font)
+    push_called = true
+  end
+  ImGui.PopFont = function(_)
+    pop_called = true
+  end
+
+  local old_reaper = reaper or {}
+  local error_function_called = false
+  reaper.ShowConsoleMsg = function(_)
+    error_function_called = true
+  end
+
+  local function_called = false
+  Fonts.wrap("context", "font", function()
+    function_called = true
+  end)
+
+  lu.assertTrue(push_called)
+  lu.assertTrue(pop_called)
+  lu.assertTrue(function_called)
+  lu.assertTrue(function_called)
+
+  ImGui = old_imgui
+  reaper = old_reaper
+end
+
+function TestFonts:testWrapErrorHandler()
+  local old_imgui = ImGui
+
+  local push_called = false
+  local pop_called = false
+  ImGui.PushFont = function(_, font)
+    push_called = true
+  end
+  ImGui.PopFont = function()
+    pop_called = true
+  end
+
+  local function_called = false
+  local error_called = false
+  Fonts.wrap("context", "font", function()
+    function_called = true
+    error("test error")
+  end, function(f)
+    return xpcall(f, function(msg)
+      error_called = true
+      lu.assertEquals(msg, "test error")
+    end)
+  end)
+
+  lu.assertTrue(push_called)
+  lu.assertTrue(pop_called)
+  lu.assertTrue(function_called)
+  lu.assertTrue(error_called)
+
+  ImGui = old_imgui
+end
+
+function TestFonts:testErrorHandlerDefault()
+  local old_imgui = ImGui
+  local old_reaper = reaper
+
+  local push_called = false
+  ImGui.PushFont = function(_, font)
+    push_called = true
+  end
+
+  local pop_called = false
+  ImGui.PopFont = function()
+    pop_called = true
+  end
+
+  local error_called = false
+  reaper.ShowConsoleMsg = function(msg)
+    error_called = true
+    lu.assertEquals(msg, 'test error')
+  end
+
+  local function_called = false
+  Fonts.wrap("context", "font", function()
+    function_called = true
+    error("test error")
+  end)
+
+  lu.assertTrue(push_called)
+  lu.assertTrue(pop_called)
+  lu.assertTrue(function_called)
+  lu.assertTrue(error_called)
+
+  ImGui = old_imgui
+  reaper = old_reaper
+end
+
+--
+
+os.exit(lu.LuaUnit.run())
