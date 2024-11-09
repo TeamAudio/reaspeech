1e4582c505a11a2de4d1c3990ebcf01a9a273ee9
Michael DeFreitas <mikeylove@gmail.com>
2024-11-09 13:17:51 -0800

    add Tween usage section in module header

--- reascripts/ReaSpeech/source/Tween.lua
+++ reascripts/ReaSpeech/source/Tween.lua
@@ -2,6 +2,35 @@
 
   Tween.lua - basic tweens for animation
 
+  Usage:
+    local tween -- an instance of a Tween(...)
+        , value -- calling tween() will return the current value
+
+    -- use a predefined tween function
+
+    local start_value = 0
+    local end_value = 1
+    local duration = 0.2
+    local on_end = function()
+      -- optionally do something when the tween is done
+    end
+
+    tween = Tween.linear(start_value, end_value, duration, on_end)
+
+    value = tween()
+
+    -- or define your own tween function like the module does
+
+    local time_function = function() return 0 end
+      -- default: ...or reaper and reaper.time_precise
+
+    local my_linear_tween = Tween(function(t, b, c, d)
+      return b + c * t / d
+    end, time_function)
+
+    tween = my_linear_tween(0.0, 1.0, 0.2, on_end)
+
+    value = tween()
 ]]--
 
 Tween = {
--- reascripts/ReaSpeech/tests/TestTween.lua
+++ reascripts/ReaSpeech/tests/TestTween.lua
@@ -2,17 +2,17 @@ package.path = '../common/libs/?.lua;../common/vendor/?.lua;' .. package.path
 
 local lu = require('luaunit')
 
--- require('mock_reaper')
 require('Polo')
 require('Storage')
 require('source/Logging')
-require('source/Tween')
 
 reaper = {
   time_precise = function()
     return 0
   end
 }
+
+require('source/Tween')
 
 --
 
@@ -61,6 +61,37 @@ function TestTween:testCallback()
   time = 1
   lu.assertEquals(tween(), 10)
   lu.assertEquals(callback_called, true)
+end
+
+function TestTween:testDocumentation()
+  local tween -- an instance of a Tween(...)
+      , value -- calling tween() will return the current value
+
+  -- use a predefined tween function
+
+  local start_value = 0
+  local end_value = 1
+  local duration = 0.2
+  local on_end = function()
+    -- optionally do something when the tween is done
+  end
+
+  tween = Tween.linear(start_value, end_value, duration, on_end)
+
+  value = tween()
+
+  -- or define your own tween function like the module does
+
+  local time_function = function() return 0 end
+    -- default: ...or reaper and reaper.time_precise
+
+  local my_linear_tween = Tween(function(t, b, c, d)
+    return b + c * t / d
+  end, time_function)
+
+  tween = my_linear_tween(0.0, 1.0, 0.2, on_end)
+
+  value = tween()
 end
 
 --
