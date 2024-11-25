91fd2c356bf3cf2718d14d704729d863a6a1ede8
Michael DeFreitas <mikeylove@gmail.com>
2024-11-25 12:17:04 -0800

    small PathUtil module
    feels like we're going to be doing more of these kinds of interactions, so it seemed helpful to start a module where we can iron that out.

---
+++ reascripts/common/libs/PathUtil.lua
@@ -0,0 +1,91 @@
+--[[
+
+  PathUtil.lua - Path utilities for ReaScript
+
+]]--
+
+PathUtil = {}
+
+-- Apply an extension to a file path, if it doesn't already have one.
+-- Empty or nil path returns the empty string.
+PathUtil.apply_extension = function(filepath, extension)
+  if not filepath or #filepath < 1 then
+    return ""
+  end
+
+  local path = ""
+
+  local path_part, file_part = filepath:match("(.+)[\\/](.+)")
+
+  if path_part then
+    path = path_part .. PathUtil._path_separator()
+    filepath = file_part
+  end
+
+  -- split the path by ".", if last chunk is alphanumeric don't apply extension
+  local chunks = {}
+  for chunk in filepath:gmatch("[^%.]+") do
+    table.insert(chunks, chunk)
+  end
+
+  local last_chunk = chunks[#chunks]
+
+  if #chunks > 1 and last_chunk:find("%w+") then
+    return path .. filepath
+  end
+
+  return path .. filepath .. '.' .. extension
+end
+
+-- Returns the given path if a full path, otherwise returns given path
+-- relative to the REAPER project resource directory.
+PathUtil.get_real_path = function(path_arg)
+  if PathUtil._is_full_path(path_arg) then
+    return path_arg
+  end
+
+  local project_path = reaper.GetProjectPath()
+  return project_path .. PathUtil._path_separator() .. path_arg
+end
+
+-- Returns the command to reveal the given path in the OS file explorer.
+-- Works for Windows and macOS. To quote a wise philosopher, "Linux is a mess."
+PathUtil.get_reveal_command = function(path_arg)
+  if PathUtil._is_windows() then
+    return 'explorer /select,"' .. path_arg .. '"'
+  elseif PathUtil._is_mac() then
+    return 'open -R "' .. path_arg .. '"'
+  end
+
+  return ""
+end
+
+PathUtil._is_full_path = function(path)
+  local found
+
+  if PathUtil._is_windows() then
+    found = path:find("^%w:\\")
+  else
+    found = path:find("^/")
+  end
+
+  return found and true or false
+end
+
+PathUtil._is_mac = function()
+  local _os = reaper.GetOS()
+  local is_mac = _os:find("OSX", 1, true) or _os:find("macOS", 1, true)
+  return is_mac and true or false
+end
+
+PathUtil._is_windows = function()
+  return reaper.GetOS():find("Win") and true or false
+end
+
+PathUtil._path_separator = function()
+  if PathUtil._is_windows() then
+    return "\\"
+  else
+    return "/"
+  end
+end
---
+++ reascripts/common/tests/TestPathUtil.lua
@@ -0,0 +1,163 @@
+package.path = '../common/libs/?.lua;../common/vendor/?.lua;' .. package.path
+
+local lu = require('luaunit')
+
+require('PathUtil')
+
+TestPathUtil = {
+  PROJECT_PATH = "Z:\\reaper-project-path",
+}
+
+reaper = {
+  GetOS = function() return 'Win64' end,
+  GetProjectPath = function() return TestPathUtil.PROJECT_PATH end,
+}
+
+function TestPathUtil:testApplyExtension()
+  local extension = "json"
+
+  local _assert_applied = function(p)
+    lu.assertEquals(PathUtil.apply_extension(p, extension), p .. '.' .. extension)
+  end
+
+  local _assert_unapplied = function(p)
+    lu.assertEquals(PathUtil.apply_extension(p, extension), p)
+  end
+
+  -- blank in, blank out
+  lu.assertEquals(PathUtil.apply_extension("", extension), "")
+
+  for _, p in ipairs({
+    '\\',
+    '\\\\',
+    'some-file',
+    'path\\to\\some-file',
+    'C:\\path\\to\\some-file'
+  }) do
+    _assert_applied(p)
+  end
+
+  for _, p in ipairs({
+    'some-file.json',
+    'path\\to\\some-file.json',
+    'C:\\path\\to\\some-file.json'
+  }) do
+    _assert_unapplied(p)
+  end
+
+  for _, os_string in ipairs({"OSX64", "Other"}) do
+    reaper.GetOS = function() return os_string end
+
+    for _, p in ipairs({
+      '/',
+      '//',
+      'some-file',
+      'path/to/some-file',
+      '/path/to/some-file'
+    }) do
+      _assert_applied(p)
+    end
+
+    for _, p in ipairs({
+      'some-file.json',
+      'path/to/some-file.json',
+      '/path/to/some-file.json'
+    }) do
+      _assert_unapplied(p)
+    end
+  end
+end
+
+function TestPathUtil:testGetRealPathForFullPathArg()
+  reaper.GetProjectPath = function()
+    return self.PROJECT_PATH
+  end
+
+  reaper.GetOS = function() return 'Win64' end
+
+  local path_arg = "path\\to\\some-file.json"
+
+  lu.assertEquals(PathUtil.get_real_path(path_arg), self.PROJECT_PATH .. "\\" .. path_arg)
+
+  path_arg = "C:\\path\\to\\some-file.json"
+
+  lu.assertEquals(PathUtil.get_real_path(path_arg), path_arg)
+end
+
+function TestPathUtil:testGetRevealCommand()
+  local windows_path = "C:\\path\\to\\some-file.json"
+  local windows_expectation = 'explorer /select,"' .. windows_path .. '"'
+
+  local mac_and_other_path = "/path/to/some-file.json"
+  local mac_expectation = 'open -R "' .. mac_and_other_path .. '"'
+  local other_expectation = ""
+
+  reaper.GetOS = function() return "Win64" end
+  lu.assertEquals(PathUtil.get_reveal_command(windows_path), windows_expectation)
+
+  reaper.GetOS = function() return "OSX64" end
+  lu.assertEquals(PathUtil.get_reveal_command(mac_and_other_path), mac_expectation)
+
+  reaper.GetOS = function() return "Other" end
+  lu.assertEquals(PathUtil.get_reveal_command(mac_and_other_path), other_expectation)
+end
+
+function TestPathUtil:testIsFullPath()
+  local os_returnval = 'Win64'
+  reaper.GetOS = function() return os_returnval end
+
+  local assertions = {
+    [true] = {
+      "C:\\path\\to\\some-file.json",
+      "Z:\\path\\to\\some-file.json",
+    },
+    [false] = {
+      "path\\to\\some-file.json",
+      "some-file.json",
+    },
+  }
+
+  for expected, path in pairs(assertions) do
+    for _, p in ipairs(path) do
+      lu.assertEquals(PathUtil._is_full_path(p), expected)
+    end
+  end
+
+  for _, os_string in ipairs({"OSX64", "Other"}) do
+    os_returnval = os_string
+    assertions = {
+      [true] = {
+        "/path/to/some-file.json",
+      },
+      [false] = {
+        "path/to/some-file.json",
+        "some-file.json",
+      },
+    }
+
+    for expected, path in pairs(assertions) do
+      for _, p in ipairs(path) do
+        lu.assertEquals(PathUtil._is_full_path(p), expected)
+      end
+    end
+  end
+end
+
+function TestPathUtil:testPathSeparator()
+  local os_returnval
+  reaper.GetOS = function() return os_returnval end
+
+  local assertions = {
+    ["\\"] = {"Win32", "Win64"},
+    ["/"] = {"OSX32", "OSX64", "macOS-arm64", "Other" },
+  }
+
+  for separator, os_strings in pairs(assertions) do
+    for _, os_string in ipairs(os_strings) do
+      os_returnval = os_string
+      lu.assertEquals(PathUtil._path_separator(), separator)
+    end
+  end
+end
+
+os.exit(lu.LuaUnit.run())
