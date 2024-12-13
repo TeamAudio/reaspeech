cf5729352ae626ed8cb3cd0ecf5586a5e4154be1
Dave Benjamin <dave@teamaud.io>
2024-12-13 08:58:05 -0700

    Add ImGui context builder

--- reascripts/ReaSpeech/source/ReaSpeechMain.lua
+++ reascripts/ReaSpeech/source/ReaSpeechMain.lua
@@ -11,29 +11,25 @@ ReaSpeechMain = {}
 
 function ReaSpeechMain:main()
   if not self:check_imgui() then return end
-  reaper.atexit(function () self:onexit() end)
+  reaper.atexit(function () self:on_exit() end)
 
-  self:build_ctx()
+  self:init_ctx()
+  ctx = Ctx()
+
   Theme:init()
   app = ReaSpeechUI.new()
   app:present()
+
   reaper.defer(self:loop())
 end
 
 function ReaSpeechMain:loop()
   return function()
     if app:presenting() or app:is_open() then
-      self:build_ctx()
+      ctx = Ctx()
       Trap(function() app:react() end)
       reaper.defer(self:loop())
     end
-  end
-end
-
-function ReaSpeechMain:build_ctx()
-  if not ImGui.ValidatePtr(ctx, 'ImGui_Context*') then
-    ctx = ImGui.CreateContext(ReaSpeechUI.TITLE, ReaSpeechUI.config_flags())
-    Fonts:init(ctx)
   end
 end
 
@@ -51,6 +47,14 @@ function ReaSpeechMain:check_imgui()
   end
 end
 
-function ReaSpeechMain:onexit()
+function ReaSpeechMain:init_ctx()
+  Ctx.label = ReaSpeechUI.TITLE
+  Ctx.flags = ReaSpeechUI.config_flags()
+  Ctx.on_create = function (ctx)
+    Fonts:init(ctx)
+  end
+end
+
+function ReaSpeechMain:on_exit()
   Tempfile:remove_all()
 end
