b67d901bdf538e01bd33b02f44152ad938e36b38
Michael DeFreitas <mikeylove@gmail.com>
2025-05-14 17:19:47 -0700

    first draft implementing mlx-whisper

--- reascripts/ReaSpeech/source/ui/WhisperModels.lua
+++ reascripts/ReaSpeech/source/ui/WhisperModels.lua
@@ -6,23 +6,26 @@
 
 WhisperModels = {
   MODELS = {
-    { name = 'tiny', label = 'Tiny' },
-    { name = 'tiny.en', label = 'Tiny', lang = 'en' },
-    { name = 'base', label = 'Base' },
-    { name = 'base.en', label = 'Base', lang = 'en' },
-    { name = 'small', label = 'Small' },
-    { name = 'small.en', label = 'Small', lang = 'en' },
-    { name = 'medium', label = 'Medium' },
-    { name = 'medium.en', label = 'Medium', lang = 'en' },
-    { name = 'large', label = 'Large' },
-    { name = 'large-v1', label = 'Large v1' },
-    { name = 'large-v2', label = 'Large v2' },
-    { name = 'large-v3', label = 'Large v3' },
-    { name = 'large-v3-turbo', label = 'Large v3 Turbo' },
+    { name = 'tiny', label = 'Tiny', not_engine = 'mlx_whisper' },
+    { name = 'tiny.en', label = 'Tiny', lang = 'en', not_engine = 'mlx_whisper' },
+    { name = 'base', label = 'Base', not_engine = 'mlx_whisper' },
+    { name = 'base.en', label = 'Base', lang = 'en', not_engine = 'mlx_whisper' },
+    { name = 'small', label = 'Small', not_engine = 'mlx_whisper' },
+    { name = 'small.en', label = 'Small', lang = 'en', not_engine = 'mlx_whisper' },
+    { name = 'medium', label = 'Medium', not_engine = 'mlx_whisper' },
+    { name = 'medium.en', label = 'Medium', lang = 'en', not_engine = 'mlx_whisper' },
+    { name = 'large', label = 'Large', not_engine = 'mlx_whisper' },
+    { name = 'large-v1', label = 'Large v1', not_engine = 'mlx_whisper' },
+    { name = 'large-v2', label = 'Large v2', not_engine = 'mlx_whisper' },
+    { name = 'large-v3', label = 'Large v3', not_engine = 'mlx_whisper' },
+    { name = 'large-v3-turbo', label = 'Large v3 Turbo', not_engine = 'mlx_whisper' },
     { name = 'distil-small.en', label = 'Distil Small', lang = 'en', engine = 'faster_whisper' },
     { name = 'distil-medium.en', label = 'Distil Medium', lang = 'en', engine = 'faster_whisper' },
     { name = 'distil-large-v2', label = 'Distil Large v2', lang = 'en', engine = 'faster_whisper' },
     { name = 'distil-large-v3', label = 'Distil Large v3', lang = 'en', engine = 'faster_whisper' },
+    { name = 'mlx-community/whisper-small-mlx', label = 'MLX Small', lang = 'en', engine = 'mlx_whisper' },
+    { name = 'mlx-community/whisper-medium-mlx', label = 'MLX Medium', lang = 'en', engine = 'mlx_whisper' },
+    { name = 'mlx-community/whisper-large-mlx', label = 'MLX Large', lang = 'en', engine = 'mlx_whisper' },
   },
 }
 
@@ -43,7 +46,9 @@ function WhisperModels.get_model_names(engine)
         table.insert(names, model.name)
       end
     else
-      table.insert(names, model.name)
+      if model.not_engine ~= engine then
+        table.insert(names, model.name)
+      end
     end
   end
 
