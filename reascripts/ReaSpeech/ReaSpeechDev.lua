f15a7f85a55a84082d0cc1bb7b50420da952a83a
Dave Benjamin <dave@teamaud.io>
2026-07-30 20:29:34 -0700

    Migrate ReaSpeechDev.lua

--- reascripts/ReaSpeech/ReaSpeechDev.lua
+++ reascripts/ReaSpeech/ReaSpeechDev.lua
@@ -1,6 +1,6 @@
 --[[
 
-ReaSpeechDev.lua - ReaSpeech UI development version
+ReaSpeechDev.lua - ReaSpeech development entry point
 
 ]]--
 
@@ -9,21 +9,12 @@ local script_path, _ = ({reaper.get_action_context()})[2]:match("(.-)([^/\\]+).l
 local function recursive_dofile(dir)
   local source_file, source_index = '', 0
 
-  local initializer = dir .. '/_module.lua'
-  if reaper.file_exists(initializer) then
-    reaper.ShowConsoleMsg(initializer .. '\n')
-    dofile(initializer)
-  end
-
   while (source_file ~= nil) do
     source_file = reaper.EnumerateFiles(dir, source_index)
     if source_file and source_file:sub(-4) == '.lua' then
       local source_path = dir .. '/' .. source_file
-
-      if source_path ~= initializer then
-        reaper.ShowConsoleMsg(source_path .. '\n')
-        dofile(source_path)
-      end
+      reaper.ShowConsoleMsg(source_path .. '\n')
+      dofile(source_path)
     end
     source_index = source_index + 1
   end
@@ -39,22 +30,13 @@ local function recursive_dofile(dir)
 end
 
 dofile(script_path .. 'source/include/globals.lua')
+dofile(script_path .. 'vendor/json.lua')
 
-for _, source_dir in pairs({'../vendor', '../resources', 'libs', 'ui', 'main'}) do
+recursive_dofile(script_path .. 'resources/images')
+
+for _, source_dir in ipairs({'libs', 'ui', 'main'}) do
   recursive_dofile(script_path .. 'source/' .. source_dir)
 end
 
--- We're not inside of docker! We're undocked!
-Script = {
-  name = "ReaSpeechDev",
-  host = "localhost:9000",
-  protocol = "http:",
-  env = "production",
-  lua = _VERSION:match('[%d.]+'),
-  timeout = 30000,
-}
-
+dofile(script_path .. 'version.lua')
 dofile(script_path .. 'source/include/main.lua')
-
--- Uncomment the following line to load example response data
--- dofile(script_path .. 'examples/response_data_json.lua')
