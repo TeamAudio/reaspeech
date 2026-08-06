36e1564093da69135371d6e4f8327d663982fc63
Dave Benjamin <dave@teamaud.io>
2026-08-06 08:26:54 -0700

    More helpful ReaSpeech Lib instructions

--- reascripts/ReaSpeech/source/main/ReaSpeechMain.lua
+++ reascripts/ReaSpeech/source/main/ReaSpeechMain.lua
@@ -10,13 +10,17 @@ ReaSpeechMain = {}
 
 function ReaSpeechMain:main()
   if not self:check_imgui() then return end
-  if not self:check_reaspeech_lib() then return end
   reaper.atexit(function () self:on_exit() end)
 
   self:init_logging()
   self:init_ctx()
-
   Theme:init()
+
+  if not self:check_reaspeech_lib() then
+    self:show_reaspeech_lib_required()
+    return
+  end
+
   app = ReaSpeechUI.new()
   app:present()
 
@@ -24,17 +28,41 @@ function ReaSpeechMain:main()
 end
 
 function ReaSpeechMain:check_reaspeech_lib()
-  if reaper.ReaSpeech_StartEx and reaper.ReaSpeech_Poll and reaper.ReaSpeech_Cancel then
-    return true
+  return reaper.ReaSpeech_StartEx
+    and reaper.ReaSpeech_Poll
+    and reaper.ReaSpeech_Cancel
+end
+
+function ReaSpeechMain:show_reaspeech_lib_required()
+  local repository_url = 'https://github.com/TeamAudio/reascripts/raw/main/index.xml'
+
+  app = AlertPopup.new {
+    title = 'ReaSpeech Lib required',
+    WIDTH = 600,
+  }
+
+  app.msg = function()
+    ImGui.Text(Ctx(), 'This script requires ReaSpeech Lib, which can be installed from ReaPack.')
+    ImGui.Spacing(Ctx())
+    ImGui.Text(Ctx(), '1. If needed, import the TeamAudio repository:')
+    ImGui.SetNextItemWidth(Ctx(), -1)
+    ImGui.InputText(Ctx(), '##repository-url', repository_url, ImGui.InputTextFlags_ReadOnly())
+    if ImGui.Button(Ctx(), 'Copy repository URL') then
+      ImGui.SetClipboardText(Ctx(), repository_url)
+    end
+    ImGui.Spacing(Ctx())
+    ImGui.Text(Ctx(), '2. Install ReaSpeech Lib from:')
+    ImGui.Text(Ctx(), 'Extensions > ReaPack > Browse packages...')
+    ImGui.Spacing(Ctx())
+    ImGui.Text(Ctx(), '3. Restart REAPER after installation.')
   end
-  reaper.MB(
-    "This script requires ReaSpeech Lib.\n\n"
-    .. "Install the ReaSpeech extension in REAPER's UserPlugins directory, "
-    .. "then restart REAPER.",
-    "ReaSpeech Lib required",
-    0
-  )
-  return false
+
+  app.react = function(self)
+    self:render()
+  end
+  app:present()
+
+  reaper.defer(self:loop())
 end
 
 function ReaSpeechMain:loop()
