4881d7f642b1f2921ba3f2f83ffc0f0e8bc46e8b
Dave Benjamin <dave@teamaud.io>
2026-08-09 09:57:24 -0700

    Add GPL comment to bundle

--- reascripts/ReaSpeech/source/include/header.lua
+++ reascripts/ReaSpeech/source/include/header.lua
@@ -5,6 +5,9 @@
 -- @changelog
 --   Replaced the Docker/PyTorch/curl backend with ReaSpeech Lib.
 
+-- ReaSpeech is licensed under the GNU General Public License v3.0,
+-- except where otherwise specified.
+
 --[[
 
   ReaSpeech.lua - Speech recognition tool for Reaper
